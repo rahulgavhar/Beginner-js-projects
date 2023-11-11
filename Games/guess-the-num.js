36a7c650ad3a0a5cd84e61474855ad134af7ef37
Rahul Gavhar <138155613+rahulgavhar@users.noreply.github.com>
2023-11-11 21:53:36 +0530

    Update guess-the-num.js

--- Games/guess-the-num.js
+++ Games/guess-the-num.js
@@ -1,17 +1,41 @@
 //Guessing Game
 const prompt=require("prompt-sync")();
 // If not on a browser, use the above line & npm install prompt-sync
-let num1=Math.floor(Math.random()*100);
-let num2=prompt("Enter the number: ");
-let ii=1;
+
+function validateInput(num2){
+    if (isNaN(num2)!=true){
+        if (num2>=1 && num2<=100){
+            return true;
+        }else{
+            console.log("Please enter a number from 1-100.\n");
+            ii-=1;
+            return false;
+        }
+    
+    }
+    else{
+        console.log("Please enter a number.\n");
+        ii-=1;
+        return false;
+    }
+}
+let num1=(Math.floor(Math.random()*100))+1;
+let num2;
+let ii=0;
 while (num1!=num2){
+    num2=prompt("Guess the number(1-100): ");
+    num2=Number.parseInt(num2);
+    while(validateInput(num2)!=true){
+        num2=prompt("Guess the number(1-100): ");
+        num2=Number.parseInt(num2);
+    }
+    ii++;
     if (num1>num2){
         console.log("The number is greater than ",num2);
     }
     else{
         console.log("The number is smaller than ",num2);
     }
-    num2=prompt("Enter the number: ");
-    ii++;
+    
 }
 console.log("You have guessed the correct number in",ii,"guesses.");
